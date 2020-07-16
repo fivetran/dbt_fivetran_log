4942321b9c6a6c5f6dd12500f142f27caea2a5a3
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2020-07-16 15:15:57 -0700

    schema changes

--- models/fivetran_log_connector_status.sql
+++ models/fivetran_log_connector_status.sql
@@ -10,6 +10,20 @@ with log as (
 
 ),
 
+schema_changes as (
+
+    select
+        connector_id,
+        count(*) as number_of_schema_changes
+
+    from {{ ref('stg_fivetran_log_log') }}
+    where {{ dbt_utils.datediff('created_at', dbt_utils.current_timestamp(), 'day') }} <= 30
+    and event_subtype in ('create_table', 'alter_table', 'create_schema')
+
+    group by 1
+
+),
+
 connector as (
 
     select *
@@ -63,7 +77,6 @@ connector_recent_logs as (
         connector_health.destination_id,
         connector_health.connector_status,
         connector_health.data_sync_status,
-        -- connector_health.is_paused,
         connector_health.last_synced_at,
         log.event_subtype,
         log.event_type,
@@ -79,38 +92,45 @@ connector_recent_logs as (
 
 ),
 
-agg_connector_recent_logs as (
-
-    select
-        connector_id,
-        connector_name,
-        connector_type,
-        destination_id,
-        connector_status,
-        data_sync_status,
-        last_synced_at,
-        
-        {{  string_agg('case when event_type = "ERROR" then message_data else null end', "'\\n'") }} as errors_since_last_completed_sync,
-        {{ string_agg('case when event_type = "WARNING" then message_data else null end', "'\\n'") }} as warnings_since_last_completed_sync,
-
-
-    from connector_recent_logs
-    group by 1,2,3,4,5,6,7
-)
-,
-
 final as (
 
-    select 
-        connector_id,
-        connector_name,
-        connector_type,
-        destination_id,
-        connector_status,
-        data_sync_status,
-        last_synced_at
-    
-    from agg_connector_recent_logs
-)
+    select
+        connector_recent_logs.connector_id,
+        connector_recent_logs.connector_name,
+        connector_recent_logs.connector_type,
+        connector_recent_logs.destination_id,
+        connector_recent_logs.connector_status,
+        connector_recent_logs.data_sync_status,
+        connector_recent_logs.last_synced_at,
+        coalesce(schema_changes.number_of_schema_changes, 0) as number_of_schema_changes_last_month,
+        
+        -- need some data with errors/warnings to test.... TODO
+        {{ string_agg('case when connector_recent_logs.event_type = "ERROR" then connector_recent_logs.message_data else null end', "'\\n'") }} as errors_since_last_completed_sync,
+        {{ string_agg('case when connector_recent_logs.event_type = "WARNING" then connector_recent_logs.message_data else null end', "'\\n'") }} as warnings_since_last_completed_sync
+        
 
-select * from agg_connector_recent_logs
+    from connector_recent_logs
+    left join schema_changes 
+        on connector_recent_logs.connector_name = schema_changes.connector_id -- TODO: change when bug is fixed
+    group by 1,2,3,4,5,6,7,8
+)
+-- ,
+
+-- final as (
+
+--     select 
+--         agg_connector_recent_logs.connector_id,
+--         agg_connector_recent_logs.connector_name,
+--         agg_connector_recent_logs.connector_type,
+--         agg_connector_recent_logs.destination_id,
+--         agg_connector_recent_logs.connector_status,
+--         agg_connector_recent_logs.data_sync_status,
+--         agg_connector_recent_logs.last_synced_at,
+--         coalesce(schema_changes.number_of_schema_changes, 0) as number_of_schema_changes_last_month
+    
+--     from agg_connector_recent_logs
+--     left join schema_changes 
+--         on agg_connector_recent_logs.connector_name = schema_changes.connector_id -- TODO: change when bug is fixed
+-- )
+
+select * from final

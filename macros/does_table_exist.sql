125dbff987657efe973e4b497bb31e19bf1ef084
fivetran-reneeli <renee.li@fivetran.com>
2022-04-18 11:51:09 -0500

    add comments to macro

--- macros/does_table_exist.sql
+++ macros/does_table_exist.sql
@@ -1,16 +1,16 @@
 {%- macro does_table_exist(table_name) -%}
-    {%- if execute -%}
-    {%- set ns = namespace(has_table=false) -%}
-        {%- for node in graph.sources.values() -%}
+    {%- if execute -%} -- returns true when dbt is in execute mode
+    {%- set ns = namespace(has_table=false) -%} -- declare boolean namespace and default value 
+        {%- for node in graph.sources.values() -%} -- grab sources from the dictionary of nodes 
         -- call the database for the matching table
             {%- set source_relation = adapter.get_relation(
                     database=node.database,
                     schema=node.schema,
                     identifier=node.name ) -%} 
             {%- if source_relation == None and node.name == table_name -%} 
-                {{ return(False) }}
+                {{ return(False) }} -- return false if relation identified by the database.schema.identifier does not exist for the given table name
             {%- elif source_relation != None and node.name == table_name -%} 
-                {{ return(True) }}
+                {{ return(True) }} -- otherwise return True 
             {% endif %}
         {%- endfor -%}
     {%- endif -%} 

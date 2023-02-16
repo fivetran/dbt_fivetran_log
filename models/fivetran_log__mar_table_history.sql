b456cc7344efcb04451734bc5b57660a03fb956a
Simon Stepper <simon.stepper@carta.com>
2023-02-16 11:09:27 +0100

    fix: aggregate by month in fivetra_log__mar_table_history

--- models/fivetran_log__mar_table_history.sql
+++ models/fivetran_log__mar_table_history.sql
@@ -1,16 +1,16 @@
 with incremental_mar as (
 
-    select 
-        *, 
+    select
+        *,
         {{ dbt.date_trunc('month', 'measured_date') }} as measured_month
 
-    from {{ ref('stg_fivetran_log__incremental_mar') }} 
+    from {{ ref('stg_fivetran_log__incremental_mar') }}
 
 ),
 
 connector as (
 
-    select * 
+    select *
     from {{ ref('stg_fivetran_log__connector') }}
 ),
 
@@ -27,50 +27,37 @@ ordered_mar as (
         schema_name,
         table_name,
         destination_id,
-        measured_date,
         measured_month,
-        incremental_rows,
-        coalesce(case when lower(free_type) = 'paid'
+        max(measured_date) as last_measured_at,
+        sum(incremental_rows) as incremental_rows,
+        sum(coalesce(case when lower(free_type) = 'paid'
             then incremental_rows
-        end, 0) as paid_monthly_active_rows,
-        coalesce(case when lower(free_type) != 'paid'
+        end, 0)) as paid_monthly_active_rows,
+        sum(coalesce(case when lower(free_type) != 'paid'
             then incremental_rows
-        end, 0) as free_monthly_active_rows
+        end, 0)) as free_monthly_active_rows,
+        (free_monthly_active_rows + paid_monthly_active_rows) as total_monthly_active_rows
 
     from incremental_mar
-
-),
-
-latest_mar as (
-    select 
-        connector_name,
-        schema_name,
-        table_name,
-        destination_id,
-        measured_month,
-        date(measured_date) as last_measured_at,
-        free_monthly_active_rows,
-        paid_monthly_active_rows,
-        (free_monthly_active_rows + paid_monthly_active_rows) as total_monthly_active_rows
-    
-    from ordered_mar
+    {{dbt_utils.group_by(5)}}
 
 ),
 
 mar_join as (
 
-    select 
-        latest_mar.*,
+    select
+        ordered_mar.*,
         connector.connector_type,
         connector.connector_id,
         destination.destination_name
 
-    from latest_mar
-    join connector 
-        on latest_mar.connector_name = connector.connector_name
-        and latest_mar.destination_id = connector.destination_id
-    join destination on latest_mar.destination_id = destination.destination_id
+    from ordered_mar
+    join connector
+        on ordered_mar.connector_name = connector.connector_name
+        and ordered_mar.destination_id = connector.destination_id
+    join destination on ordered_mar.destination_id = destination.destination_id
 )
 
 select * from mar_join
 order by measured_month desc, destination_id, connector_name
+

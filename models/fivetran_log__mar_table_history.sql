9227223be0c20519b54aefef36f37bcca4bf7882
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2023-02-16 10:12:09 -0600

    jm review updates

--- models/fivetran_log__mar_table_history.sql
+++ models/fivetran_log__mar_table_history.sql
@@ -5,7 +5,6 @@ with incremental_mar as (
         {{ dbt.date_trunc('month', 'measured_date') }} as measured_month
 
     from {{ ref('stg_fivetran_log__incremental_mar') }}
-
 ),
 
 connector as (
@@ -35,27 +34,40 @@ ordered_mar as (
         end, 0)) as paid_monthly_active_rows,
         sum(coalesce(case when lower(free_type) != 'paid'
             then incremental_rows
-        end, 0)) as free_monthly_active_rows,
-        (free_monthly_active_rows + paid_monthly_active_rows) as total_monthly_active_rows
+        end, 0)) as free_monthly_active_rows
 
     from incremental_mar
     {{dbt_utils.group_by(5)}}
+),
 
+latest_mar as (
+    select 
+        connector_name,
+        schema_name,
+        table_name,
+        destination_id,
+        measured_month,
+        date(last_measured_at) as last_measured_at,
+        free_monthly_active_rows,
+        paid_monthly_active_rows,
+        (free_monthly_active_rows + paid_monthly_active_rows) as total_monthly_active_rows
+
+    from ordered_mar
 ),
 
 mar_join as (
 
     select
-        ordered_mar.*,
+        latest_mar.*,
         connector.connector_type,
         connector.connector_id,
         destination.destination_name
 
-    from ordered_mar
+    from latest_mar
     join connector
-        on ordered_mar.connector_name = connector.connector_name
-        and ordered_mar.destination_id = connector.destination_id
-    join destination on ordered_mar.destination_id = destination.destination_id
+        on latest_mar.connector_name = connector.connector_name
+        and latest_mar.destination_id = connector.destination_id
+    join destination on latest_mar.destination_id = destination.destination_id
 )
 
 select * from mar_join

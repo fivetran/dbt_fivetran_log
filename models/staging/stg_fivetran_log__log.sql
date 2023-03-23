5a60ed01a14f666eb35ad6e7e832c3875800fd6b
camcyr-at-brzwy <cameron.cyr@breezeway.io>
2023-03-23 09:35:21 -0500

    adds sync_id as well as docs

--- models/staging/stg_fivetran_log__log.sql
+++ models/staging/stg_fivetran_log__log.sql
@@ -8,6 +8,7 @@ fields as (
 
     select
         id as log_id, 
+        sync_id,
         cast(time_stamp as {{ dbt.type_timestamp() }}) as created_at,
         connector_id, -- Note: the connector_id column used to erroneously equal the connector_name, NOT its id.
         case when transformation_id is not null and event is null then 'TRANSFORMATION'

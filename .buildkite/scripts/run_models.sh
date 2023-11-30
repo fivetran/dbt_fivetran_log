5021033923130371e4d222ce83c801ec20d28374
Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
2023-11-30 11:44:07 -0800

    check which version of debian

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -22,7 +22,8 @@ if [ "$1" == "sqlserver" ]; then
     pip install --no-cache-dir --no-binary :all: pyodbc==4.0.39
     which odbcinst
     odbcinst -j
-    cat /home/linuxbrew/.linuxbrew/etc/odbc.ini
+    # cat /home/linuxbrew/.linuxbrew/etc/odbc.ini
+    lsb_release -a
 else
     pip install -r integration_tests/requirements.txt
 fi

a49d730903a37aa9d50be509d1b0fc4c0544a523
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-12-14 00:04:48 +0100

    revise macro

--- macros/fivetran_log_json_extract.sql
+++ macros/fivetran_log_json_extract.sql
@@ -6,36 +6,12 @@
 
 {% macro default__fivetran_log_json_extract(string, string_path) %}
 
-  json_extract_path_text({{string}}, {{ "'" ~ string_path ~ "'" }} )
- 
-{% endmacro %}
-
-{% macro snowflake__fivetran_log_json_extract(string, string_path) %}
-
-  json_extract_path_text(try_parse_json( {{string}} ), {{ "'" ~ string_path ~ "'" }} )
-
-{% endmacro %}
-
-{% macro redshift__fivetran_log_json_extract(string, string_path) %}
-
-  case when is_valid_json( {{string}} ) then json_extract_path_text({{string}}, {{ "'" ~ string_path ~ "'" }} ) else null end
- 
-{% endmacro %}
-
-{% macro bigquery__fivetran_log_json_extract(string, string_path) %}
-
-  json_extract_scalar({{string}}, {{ "'$." ~ string_path ~ "'" }} )
-
-{% endmacro %}
-
-{% macro postgres__fivetran_log_json_extract(string, string_path) %}
-
-  {{string}}::json->>{{"'" ~ string_path ~ "'" }}
+  {{ fivetran_utils.json_extract(string=string, string_path=string_path) }}
 
 {% endmacro %}
 
 {% macro spark__fivetran_log_json_extract(string, string_path) %}
 
-    get_json_object({{string}}, '$.{{string_path}}')
+  get_json_object({{string}}, '$.{{string_path}}')
 
 {% endmacro %}

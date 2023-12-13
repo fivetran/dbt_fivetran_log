b3fb00d966bf18774b67b71db5a5525d4e88819e
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-12-14 00:13:14 +0100

    update seed

--- macros/fivetran_log_json_extract.sql
+++ macros/fivetran_log_json_extract.sql
@@ -6,18 +6,12 @@
 
 {% macro default__fivetran_log_json_extract(string, string_path) %}
 
-  {{ fivetran_utils.json_parse(string=string, string_path=string_path) }}
-
-{% endmacro %}
-
-{% macro snowflake__fivetran_log_json_extract(string, string_path) %}
-
   {{ fivetran_utils.json_extract(string=string, string_path=string_path) }}
 
 {% endmacro %}
 
-{# {% macro spark__fivetran_log_json_extract(string, string_path) %}
+{% macro spark__fivetran_log_json_extract(string, string_path) %}
 
   get_json_object({{string}}, '$.{{string_path}}')
 
-{% endmacro %} #}
+{% endmacro %}

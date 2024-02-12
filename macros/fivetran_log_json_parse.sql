9ffff41ccfc6e603dc488376b779729b5d0adaad
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-02-12 17:30:32 -0600

    updates

--- macros/fivetran_log_json_parse.sql
+++ macros/fivetran_log_json_parse.sql
@@ -6,6 +6,27 @@
 
 {% macro default__fivetran_log_json_parse(string, string_path) %}
 
+  {{ fivetran_utils.json_parse(string=string, string_path=string_path) }}
+
+{% endmacro %}
+
+{% macro snowflake__fivetran_log_json_parse(string, string_path) %}
+
+  try_parse_json( {{string}} ) {%- for s in string_path -%}{% if s is number %}[{{ s }}]{% else %}['{{ s }}']{% endif %}{%- endfor -%}
+
+{% endmacro %}
+
+{% macro redshift__fivetran_log_json_parse(string, string_path) %}
+
+  json_extract_path_text(
+    {{string}}, 
+    {%- for s in string_path -%}'{{ s }}'{%- if not loop.last -%},{%- endif -%}{%- endfor -%}, 
+    true ) -- flag for null_if_invalid=true
+
+{% endmacro %}
+
+{% macro postgres__fivetran_log_json_parse(string, string_path) %}
+
   {% if fromjson(string, none) is not none %}
     {{ fivetran_utils.json_parse(string=string, string_path=string_path) }}
   {% else %}

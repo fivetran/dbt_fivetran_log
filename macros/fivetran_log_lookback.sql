ac435aaaa8e7aea8ea854f073ed10a7726dc3a9e
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-02-19 09:51:22 -0600

    add bq logic

--- macros/fivetran_log_lookback.sql
+++ macros/fivetran_log_lookback.sql
@@ -13,3 +13,23 @@
         )
 
 {% endmacro %}
+
+{% macro bigquery__fivetran_log_lookback(from_date, datepart='day', interval=7, default_start_date='2010-01-01')  %}
+
+    -- Capture the latest timestamp in a call statement instead of a subquery for optimizing BQ costs on incremental runs
+    {%- call statement('max_date', fetch_result=True) -%}
+        select from_date from {{ this }}
+    {%- endcall -%}
+
+    -- load the result from the above query into a new variable
+    {%- set query_result = load_result('max_date') -%}
+
+    -- the query_result is stored as a dataframe. Therefore, we want to now store it as a singular value.
+    {%- set max_date = query_result['data'][0][0] -%}
+
+    coalesce(
+        {{ dbt.dateadd(datepart='day', interval=-7, from_date_or_timestamp="'" ~ max_date ~ "'") }}, 
+        {{ "'" ~ default_start_date ~ "'" }}
+        )
+
+{% endmacro %}

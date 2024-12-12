f31370d16580acaa0cf237c6b66bb2c8eaa3e0ad
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-12-12 14:45:47 -0600

    update macro and namings

--- models/fivetran_platform__audit_table.sql
+++ models/fivetran_platform__audit_table.sql
@@ -12,7 +12,11 @@
 
 with base as (
     
-    select *
+    select
+        connector_id,
+        created_at,
+        event_subtype, 
+        replace('message_data', 'operationType', 'operation_type') as message_data
     from {{ ref('stg_fivetran_platform__log') }}
     where event_subtype in ('sync_start', 'sync_end', 'write_to_table_start', 'write_to_table_end', 'records_modified')
 
@@ -37,7 +41,7 @@ sync_log as (
         *,
         {{ fivetran_log.fivetran_log_json_parse(string='message_data', string_path=['table']) }} as table_name,
         {{ fivetran_log.fivetran_log_json_parse(string='message_data', string_path=['schema']) }} as schema_name,
-        {{ fivetran_log.fivetran_log_json_parse(string='message_data', string_path=['operationType']) }} as operation_type,
+        {{ fivetran_log.fivetran_log_json_parse(string='message_data', string_path=['operation_type']) }} as operation_type,
         cast ({{ fivetran_log.fivetran_log_json_parse(string='message_data', string_path=['count']) }} as {{ dbt.type_bigint() }}) as row_count
     from base
     where event_subtype = 'records_modified'
--- models/fivetran_platform__connector_daily_events.sql
+++ models/fivetran_platform__connector_daily_events.sql
@@ -38,7 +38,7 @@ agg_log_events as (
                 when event_subtype = 'records_modified' 
                 then cast({{ fivetran_log.fivetran_log_json_parse(string='message_data', string_path=['count']) }} as {{ dbt.type_bigint()}} )
                 when event_subtype = 'extract_summary'
-                then cast(nullif({{ fivetran_log.fivetran_log_json_parse(string='message_data', string_path=['total_queries']) }}, '') as {{ dbt.type_bigint()}})
+                then cast({{ fivetran_log.fivetran_log_json_parse(string='message_data', string_path=['total_queries']) }} as {{ dbt.type_bigint()}})
                 else 1
                 end
             ) as count_events

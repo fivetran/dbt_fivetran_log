1c536fce3b2d66d2b340b4df896a9eb950a3af08
Renee Li <renee.li@fivetran.com>
2024-12-18 01:42:50 +0800

    fix name to platform and use most current staging template

---
+++ models/staging/stg_fivetran_platform__transformation_runs.sql
@@ -0,0 +1,41 @@
+
+with base as (
+
+    select * 
+    from {{ ref('stg_fivetran_platform__transformation_runs_tmp') }}
+),
+
+fields as (
+
+    select
+        {{
+            fivetran_utils.fill_staging_columns(
+                source_columns=adapter.get_columns_in_relation(ref('stg_fivetran_platform__transformation_runs_tmp')),
+                staging_columns=get_transformation_runs_columns()
+            )
+        }}
+        {{ fivetran_utils.source_relation(
+            union_schema_variable='fivetran_platform_union_schemas', 
+            union_database_variable='fivetran_platform_union_databases') 
+        }}
+    from base
+),
+
+final as (
+    
+    select 
+        source_relation, 
+        _fivetran_synced,
+        destination_id,
+        free_type,
+        job_id,
+        job_name,
+        measured_date,
+        model_runs,
+        project_type,
+        updated_at
+    from fields
+)
+
+select *
+from final

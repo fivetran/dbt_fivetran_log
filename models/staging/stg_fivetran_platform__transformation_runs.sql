5c821821658018b8fa12c17611d156b59f9f7a72
Renee Li <renee.li@fivetran.com>
2025-01-08 16:54:35 -0500

    add using_transformations variable to models

--- models/staging/stg_fivetran_platform__transformation_runs.sql
+++ models/staging/stg_fivetran_platform__transformation_runs.sql
@@ -1,3 +1,6 @@
+-- If you have the transformation_runs and wish to enable this model, set the fivetran_platform_using_transformations variable within your dbt_project.yml file to True.
+{{ config(enabled=var('fivetran_platform_using_transformations', False)) }}
+
 with base as (
 
     select * 
--- models/staging/tmp/stg_fivetran_platform__transformation_runs_tmp.sql
+++ models/staging/tmp/stg_fivetran_platform__transformation_runs_tmp.sql
@@ -1,2 +1,5 @@
+-- If you have the transformation_runs and wish to enable this model, set the fivetran_platform_using_transformations variable within your dbt_project.yml file to True.
+{{ config(enabled=var('fivetran_platform_using_transformations', False)) }}
+
 select *
 from {{ var('transformation_runs') }}

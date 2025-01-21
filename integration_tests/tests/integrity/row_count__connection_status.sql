0de7e340a5f4ace4c6eb0050b379e04b25ea6494
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2025-01-21 11:26:06 -0600

    Update tests

--- integration_tests/tests/integrity/row_count__connection_status.sql
+++ integration_tests/tests/integrity/row_count__connection_status.sql
@@ -8,30 +8,30 @@ with end_model as (
     select 
         connection_id,
         connection_name,
-        connection_type,
+        connector_type,
         destination_id,
         set_up_at,
         count(*) as row_count
     from {{ ref('fivetran_platform__connection_status') }}
-    group by connection_id, connection_name, connection_type, destination_id, set_up_at
+    group by connection_id, connection_name, connector_type, destination_id, set_up_at
 ),
 
 staging_model as (
     select
         connection_id,
         connection_name,
-        connection_type,
+        connector_type,
         destination_id,
         set_up_at,
         count(*) as row_count
     from {{ ref('stg_fivetran_platform__connection') }}
-    group by connection_id, connection_name, connection_type, destination_id, set_up_at
+    group by connection_id, connection_name, connector_type, destination_id, set_up_at
 )
 
 select 
     end_model.connection_id,
     end_model.connection_name,
-    end_model.connection_type,
+    end_model.connector_type,
     end_model.destination_id,
     end_model.set_up_at,
     end_model.row_count as end_model_row_count,

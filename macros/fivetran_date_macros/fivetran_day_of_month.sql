cd8198ec22d7ba0a83d4236d762bd4df59ef4b22
Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>
2025-03-12 14:48:59 -0500

    macro name update

--- macros/fivetran_date_macros/fivetran_day_of_month.sql
+++ macros/fivetran_date_macros/fivetran_day_of_month.sql
@@ -2,10 +2,10 @@
     {{ return(adapter.dispatch('fivetran_day_of_month', 'fivetran_log') (date)) }}
 {%- endmacro %}
 
-{%- macro default__day_of_month(date) -%}
+{%- macro default__fivetran_day_of_month(date) -%}
     {{ fivetran_log.fivetran_date_part('day', date) }}
 {%- endmacro %}
 
-{%- macro redshift__day_of_month(date) -%}
+{%- macro redshift__fivetran_day_of_month(date) -%}
     cast({{ fivetran_log.fivetran_date_part('day', date) }} as {{ dbt.type_bigint() }})
 {%- endmacro %}

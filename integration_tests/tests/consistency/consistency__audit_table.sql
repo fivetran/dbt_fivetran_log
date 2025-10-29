bda72dd05cf82c5cd8359adf219befeccd86d3fa
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2025-10-29 15:18:15 -0400

    working?

--- integration_tests/tests/consistency/consistency__audit_table.sql
+++ integration_tests/tests/consistency/consistency__audit_table.sql
@@ -8,7 +8,11 @@ with prod as (
     select
         connection_id,
         table_name,
-        count(*) as total_records
+        count(*) as total_records,
+        sum(sum_rows_replaced_or_inserted) as sum_rows_replaced_or_inserted,
+        sum(sum_rows_deleted) as sum_rows_deleted,
+        sum(sum_rows_updated) as sum_rows_updated
+
     from {{ target.schema }}_fivetran_platform_prod.fivetran_platform__audit_table
     group by 1, 2
 ),
@@ -17,7 +21,11 @@ dev as (
     select
         connection_id,
         table_name,
-        count(*) as total_records
+        count(*) as total_records,
+        sum(sum_rows_replaced_or_inserted) as sum_rows_replaced_or_inserted,
+        sum(sum_rows_deleted) as sum_rows_deleted,
+        sum(sum_rows_updated) as sum_rows_updated
+
     from {{ target.schema }}_fivetran_platform_dev.fivetran_platform__audit_table
     group by 1, 2
 ),
@@ -27,7 +35,14 @@ final_consistency_check as (
         prod.connection_id,
         prod.table_name,
         prod.total_records as prod_total,
-        dev.total_records as dev_total
+        dev.total_records as dev_total,
+        prod.sum_rows_replaced_or_inserted as prod_sum_rows_replaced_or_inserted,
+        dev.sum_rows_replaced_or_inserted as dev_sum_rows_replaced_or_inserted,
+        prod.sum_rows_deleted as prod_sum_rows_deleted,
+        dev.sum_rows_deleted as dev_sum_rows_deleted,
+        prod.sum_rows_updated as prod_sum_rows_updated,
+        dev.sum_rows_updated as dev_sum_rows_updated
+
     from prod
     left join dev 
         on dev.connection_id = prod.connection_id
@@ -39,6 +54,9 @@ consistency_check as (
     select *
     from final_consistency_check
     where prod_total != dev_total
+    or prod_sum_rows_replaced_or_inserted != dev_sum_rows_replaced_or_inserted
+    or prod_sum_rows_deleted != dev_sum_rows_deleted
+    or prod_sum_rows_updated != dev_sum_rows_updated
 ),
 
 -- For use when the current release changes the row count of the audit table model intentionally.
--- models/fivetran_platform__audit_table.sql
+++ models/fivetran_platform__audit_table.sql
@@ -46,26 +46,113 @@ with base as (
     {% endif %}
 ),
 
-sync_log as (
+parsed as (
     select 
-        *,
+        connection_id,
+        created_at,
+        event_subtype,
+        message_data,
         {{ fivetran_log.fivetran_log_json_parse(string='message_data', string_path=['table']) }} as table_name,
-        cast(null as {{ dbt.type_string() }}) as schema_name,
-        cast(null as {{ dbt.type_string() }}) as operation_type,
-        cast(null as {{ dbt.type_bigint() }}) as row_count
-    from base
-    where event_subtype in ('sync_start', 'sync_end', 'write_to_table_start', 'write_to_table_end')
 
-    union all
+        case 
+            when event_subtype = 'records_modified' then {{ fivetran_log.fivetran_log_json_parse(string='message_data', string_path=['schema']) }} 
+            else cast(null as {{ dbt.type_string() }})
+        end as schema_name,
+
+        case 
+            when event_subtype = 'records_modified' then {{ fivetran_log.fivetran_log_json_parse(string='message_data', string_path=['operation_type']) }} 
+            else cast(null as {{ dbt.type_string() }})
+        end as operation_type,
+
+        cast(case 
+            when event_subtype = 'records_modified' then {{ fivetran_log.fivetran_log_json_parse(string='message_data', string_path=['count']) }} 
+            else null
+        end as {{ dbt.type_bigint() }}) as row_count
+
+    from base
+),
+
+sync_timestamps as (
+
+    select
+        connection_id,
+        table_name,
+        event_subtype,
+        created_at,
+        schema_name,
+        operation_type,
+        row_count,
+
+        case 
+            when event_subtype = 'write_to_table_start' then 
+            min(case when event_subtype = 'write_to_table_end' then created_at else null end) 
+                over (partition by connection_id, table_name order by created_at ROWS between CURRENT ROW AND UNBOUNDED FOLLOWING) 
+        else null end as write_to_table_end,
+
+        case 
+            when event_subtype = 'write_to_table_start' then 
+        max(case when event_subtype = 'sync_start' then created_at else null end) 
+            over (partition by connection_id order by created_at ROWS between UNBOUNDED PRECEDING and CURRENT ROW) 
+        else null end as sync_start,
+
+        case 
+            when event_subtype = 'write_to_table_start' then 
+        min(case when event_subtype = 'sync_start' then created_at else null end) 
+            over (partition by connection_id order by created_at ROWS between CURRENT ROW AND UNBOUNDED FOLLOWING)
+        else null end as next_sync_start,
+
+        case 
+            when event_subtype = 'write_to_table_start' then 
+        min(case when event_subtype = 'sync_end' then created_at else null end) 
+            over (partition by connection_id order by created_at ROWS between CURRENT ROW AND UNBOUNDED FOLLOWING) 
+        else null end as sync_end -- coalesce with next_sync_start
+
+    from parsed
+
+),
+
+row_modifcation_counts as (
+
+    select
+        *,
+        case when event_subtype = 'write_to_table_start' then
+        sum(case when event_subtype = 'records_modified' and operation_type = 'REPLACED_OR_INSERTED' 
+                and created_at > sync_start and created_at < coalesce(sync_end, next_sync_start)
+                then row_count else 0  end)
+        over (partition by connection_id, table_name, created_at) else null end as sum_rows_replaced_or_inserted,
+
+        case when event_subtype = 'write_to_table_start' then
+        sum(case when event_subtype = 'records_modified' and operation_type = 'UPDATED' 
+                and created_at > sync_start and created_at < coalesce(sync_end, next_sync_start)
+                then row_count else 0  end)
+        over (partition by connection_id, table_name, created_at) else null end as sum_rows_updated,
+
+        case when event_subtype = 'write_to_table_start' then
+        sum(case when event_subtype = 'records_modified' and operation_type = 'DELETED' 
+                and created_at > sync_start and created_at < coalesce(sync_end, next_sync_start)
+                then row_count else 0  end)
+        over (partition by connection_id, table_name, created_at) else null end as sum_rows_deleted
+
+    from sync_timestamps
+    where event_subtype in ('write_to_table_start', 'records_modified')
+),
+
+limit_to_table_starts as (
 
     select 
-        *,
-        {{ fivetran_log.fivetran_log_json_parse(string='message_data', string_path=['table']) }} as table_name,
-        {{ fivetran_log.fivetran_log_json_parse(string='message_data', string_path=['schema']) }} as schema_name,
-        {{ fivetran_log.fivetran_log_json_parse(string='message_data', string_path=['operation_type']) }} as operation_type,
-        cast ({{ fivetran_log.fivetran_log_json_parse(string='message_data', string_path=['count']) }} as {{ dbt.type_bigint() }}) as row_count
-    from base
-    where event_subtype = 'records_modified'
+        connection_id,
+        schema_name,
+        table_name,
+        created_at as write_to_table_start,
+        write_to_table_end,
+        sync_start,
+        case when sync_end > next_sync_start then null else sync_end end as sync_end,
+        sum_rows_replaced_or_inserted,
+        sum_rows_updated,
+        sum_rows_deleted
+
+    from row_modifcation_counts 
+    where event_subtype = 'write_to_table_start'
 ),
 
 connection as (
@@ -77,97 +164,23 @@ connection as (
 add_connection_info as (
 
     select 
-        sync_log.*,
+        limit_to_table_starts.connection_id,
         connection.connection_name,
+        coalesce(limit_to_table_starts.schema_name, connection.connection_name) as schema_name,
+        table_name,
         connection.destination_id,
-        connection.destination_name
-    from sync_log 
+        connection.destination_name,
+        write_to_table_start,
+        write_to_table_end,
+        sync_start,
+        sync_end,
+        sum_rows_replaced_or_inserted,
+        sum_rows_updated,
+        sum_rows_deleted
+
+    from limit_to_table_starts 
     left join connection
-        on connection.connection_id = sync_log.connection_id
-),
-
-sync_timestamps as (
-
-    select
-        connection_id,
-        connection_name,
-        table_name,
-        event_subtype,
-        destination_id,
-        destination_name,
-        created_at as write_to_table_start,
-        min(case when event_subtype = 'write_to_table_end' then created_at else null end) 
-            over (partition by connection_id, table_name order by created_at ROWS between CURRENT ROW AND UNBOUNDED FOLLOWING) as write_to_table_end,
-
-        max(case when event_subtype = 'sync_start' then created_at else null end) 
-            over (partition by connection_id order by created_at ROWS between UNBOUNDED PRECEDING and CURRENT ROW) as sync_start,
-
-        min(case when event_subtype = 'sync_end' then created_at else null end) 
-            over (partition by connection_id order by created_at ROWS between CURRENT ROW AND UNBOUNDED FOLLOWING) as sync_end, -- coalesce with next_sync_start
-
-        min(case when event_subtype = 'sync_start' then created_at else null end) 
-            over (partition by connection_id order by created_at ROWS between CURRENT ROW AND UNBOUNDED FOLLOWING) as next_sync_start
-    from add_connection_info
-),
-
--- this will be the base for every record in the final CTE
-limit_to_table_starts as (
-
-    select *
-    from sync_timestamps 
-    where event_subtype = 'write_to_table_start'
-),
-
-records_modified_log as (
-
-    select 
-        connection_id,
-        created_at,
-        table_name,
-        schema_name,
-        operation_type,
-        row_count
-    from sync_log 
-    where event_subtype = 'records_modified'
-),
-
-sum_records_modified as (
-
-    select
-        limit_to_table_starts.connection_id,
-        limit_to_table_starts.connection_name,
-        coalesce(records_modified_log.schema_name, limit_to_table_starts.connection_name) as schema_name,
-        limit_to_table_starts.table_name,
-        limit_to_table_starts.destination_id,
-        limit_to_table_starts.destination_name,
-        limit_to_table_starts.write_to_table_start,
-        limit_to_table_starts.write_to_table_end,
-        limit_to_table_starts.sync_start,
-        case when limit_to_table_starts.sync_end > limit_to_table_starts.next_sync_start then null else limit_to_table_starts.sync_end end as sync_end,
-        sum(case when records_modified_log.operation_type = 'REPLACED_OR_INSERTED' then records_modified_log.row_count else 0 end) as sum_rows_replaced_or_inserted,
-        sum(case when records_modified_log.operation_type = 'UPDATED' then records_modified_log.row_count else 0 end) as sum_rows_updated,
-        sum(case when records_modified_log.operation_type = 'DELETED' then records_modified_log.row_count else 0 end) as sum_rows_deleted
-    from limit_to_table_starts
-    left join records_modified_log on 
-        limit_to_table_starts.connection_id = records_modified_log.connection_id
-        and limit_to_table_starts.table_name = records_modified_log.table_name
-
-        -- confine it to one sync
-        and records_modified_log.created_at > limit_to_table_starts.sync_start 
-        and records_modified_log.created_at < coalesce(limit_to_table_starts.sync_end, limit_to_table_starts.next_sync_start) 
-
-    -- explicit group by needed for SQL Server
-    group by 
-        limit_to_table_starts.connection_id,
-        limit_to_table_starts.connection_name,
-        coalesce(records_modified_log.schema_name, limit_to_table_starts.connection_name),
-        limit_to_table_starts.table_name,
-        limit_to_table_starts.destination_id,
-        limit_to_table_starts.destination_name,
-        limit_to_table_starts.write_to_table_start,
-        limit_to_table_starts.write_to_table_end,
-        limit_to_table_starts.sync_start,
-        case when limit_to_table_starts.sync_end > limit_to_table_starts.next_sync_start then null else limit_to_table_starts.sync_end end
+        on connection.connection_id = limit_to_table_starts.connection_id
 ),
 
 final as (
@@ -176,7 +189,7 @@ final as (
         *,
         {{ dbt_utils.generate_surrogate_key(['schema_name','connection_id', 'destination_id', 'table_name', 'write_to_table_start']) }} as unique_table_sync_key, -- for incremental materialization 
         cast({{ dbt.date_trunc('day', 'write_to_table_start') }} as date) as write_to_table_start_day -- for partitioning
-    from sum_records_modified
+    from add_connection_info
 )
 
 select *

d2e4b4cbeb7c20bd6167341c0cf5761902e5d51d
Savage Fivetran <sarah.savage@fivetran.com>
2026-07-07 13:41:14 -0700

    add_consistency_tests

---
+++ integration_tests/tests/consistency/consistency__sync_metrics.sql
@@ -0,0 +1,50 @@
+
+{{ config(
+    tags="fivetran_validations",
+    enabled=var('fivetran_validation_tests_enabled', false)
+) }}
+
+with prod as (
+    select
+        connection_id,
+        count(*) as total_records,
+        sum(extract_time_s) as sum_extract_time_s,
+        sum(total_time_s) as sum_total_time_s,
+        sum(row_count) as sum_row_count
+    from {{ target.schema }}_fivetran_platform_prod.fivetran_platform__sync_metrics
+    group by 1
+),
+
+dev as (
+    select
+        connection_id,
+        count(*) as total_records,
+        sum(extract_time_s) as sum_extract_time_s,
+        sum(total_time_s) as sum_total_time_s,
+        sum(row_count) as sum_row_count
+    from {{ target.schema }}_fivetran_platform_dev.fivetran_platform__sync_metrics
+    group by 1
+),
+
+final as (
+    select
+        prod.connection_id,
+        prod.total_records as prod_total,
+        dev.total_records as dev_total,
+        prod.sum_extract_time_s as prod_sum_extract_time_s,
+        dev.sum_extract_time_s as dev_sum_extract_time_s,
+        prod.sum_total_time_s as prod_sum_total_time_s,
+        dev.sum_total_time_s as dev_sum_total_time_s,
+        prod.sum_row_count as prod_sum_row_count,
+        dev.sum_row_count as dev_sum_row_count
+    from prod
+    left join dev
+        on dev.connection_id = prod.connection_id
+)
+
+select *
+from final
+where prod_total != dev_total
+    or abs(prod_sum_extract_time_s - dev_sum_extract_time_s) / nullif(prod_sum_extract_time_s, 0) > 0.01
+    or abs(prod_sum_total_time_s - dev_sum_total_time_s) / nullif(prod_sum_total_time_s, 0) > 0.01
+    or prod_sum_row_count != dev_sum_row_count
